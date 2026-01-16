558ddb56247e64e7366b2bf0ff74c684e587bd7c
Unbreathable <70802809+Unbreathable@users.noreply.github.com>
2026-01-16 21:50:33 +0100

    feat: Better healthcheck endpoint

--- main.go
+++ main.go
@@ -20,48 +20,56 @@ func main() {
 	app.Use(cors.New())
 	app.Use(logger.New())
 
-	app.Use(func(c *fiber.Ctx) error {
+	app.Route("/api", func(router fiber.Router) {
 
-		cred := c.Query("credential", "-")
-		if cred != credential {
-			return c.SendStatus(fiber.StatusUnauthorized)
-		}
+		router.Use(func(c *fiber.Ctx) error {
 
-		return c.Next()
-	})
+			cred := c.Query("credential", "-")
+			if cred != credential {
+				return c.SendStatus(fiber.StatusUnauthorized)
+			}
 
-	// Handle for managing the get token
-	app.Get("/get_token", func(c *fiber.Ctx) error {
-		token, ok := getTokenForServer()
-		if !ok {
-			return c.SendStatus(fiber.StatusNotFound)
-		}
-
-		token.Mutex.Lock()
-		defer token.Mutex.Unlock()
-
-		return c.JSON(fiber.Map{
-			"id":             token.Id,
-			"identity_token": token.Token.IdentityToken,
-			"session_token":  token.Token.SessionToken,
-			"uuid":           token.Token.UUID,
+			return c.Next()
 		})
+
+		// Handle for managing the get token
+		app.Get("/get_token", func(c *fiber.Ctx) error {
+			token, ok := getTokenForServer()
+			if !ok {
+				return c.SendStatus(fiber.StatusNotFound)
+			}
+
+			token.Mutex.Lock()
+			defer token.Mutex.Unlock()
+
+			return c.JSON(fiber.Map{
+				"id":             token.Id,
+				"identity_token": token.Token.IdentityToken,
+				"session_token":  token.Token.SessionToken,
+				"uuid":           token.Token.UUID,
+			})
+		})
+
+		// Handle renews for the tokens
+		app.Get("/renew", func(c *fiber.Ctx) error {
+			id := c.Query("id", "-")
+			i, err := strconv.Atoi(id)
+			if err != nil {
+				return c.SendStatus(fiber.StatusBadRequest)
+			}
+
+			refreshToken(i)
+			return c.SendStatus(fiber.StatusOK)
+		})
+
+		// Token for generating a new token using the API
+		app.Get("/add_new", generateToken)
 	})
 
-	// Handle renews for the tokens
-	app.Get("/renew", func(c *fiber.Ctx) error {
-		id := c.Query("id", "-")
-		i, err := strconv.Atoi(id)
-		if err != nil {
-			return c.SendStatus(fiber.StatusBadRequest)
-		}
-
-		refreshToken(i)
-		return c.SendStatus(fiber.StatusOK)
+	// Hello world handler for health checks
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello from Hytale Auth Manager!")
 	})
-
-	// Token for generating a new token using the API
-	app.Get("/add_new", generateToken)
 
 	app.Listen(os.Getenv("LISTEN"))
 }
